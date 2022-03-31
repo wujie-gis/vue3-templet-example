36bfa09a9ae48727c921c936464a0bd52e084152
wuj <1450626722@qq.com>
2022-03-31 10:30:53 +0800

    ✨ feat(): 测试store

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,11 @@ import { createRouter, createWebHashHistory } from 'vue-router';
 import type { RouteRecordRaw } from 'vue-router';
 
 // 配置路由信息
-const routes: RouteRecordRaw[] = [];
+// 配置路由信息
+const routes: RouteRecordRaw[] = [
+  // { path: '/login', component: () => import('@/views/login/index.vue') },
+  { path: '/store', component: () => import('@/views/store/index.vue') },
+];
 
 const router = createRouter({
   routes,
