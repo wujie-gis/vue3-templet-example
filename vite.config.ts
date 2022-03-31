2312fc607f2f799e3e464afd1b8240876d72fc5f
wuj <1450626722@qq.com>
2022-03-31 10:12:03 +0800

    ✨ feat(): 配置element-plus

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,12 @@
 import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
-
-// https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [vue()],
-// });
 import { resolve } from 'path';
 import type { ConfigEnv } from 'vite';
+import AutoImport from 'unplugin-auto-import/vite';
+import Components from 'unplugin-vue-components/vite';
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
 
+// https://vitejs.dev/config/
 export default defineConfig(({ mode }: ConfigEnv) => {
   const env = loadEnv(mode, process.cwd());
   return {
@@ -31,6 +30,14 @@ export default defineConfig(({ mode }: ConfigEnv) => {
         },
       },
     },
-    plugins: [vue()],
+    plugins: [
+      vue(),
+      AutoImport({
+        resolvers: [ElementPlusResolver()],
+      }),
+      Components({
+        resolvers: [ElementPlusResolver()],
+      }),
+    ],
   };
 });
