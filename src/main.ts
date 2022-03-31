dd516e79d627a9ab7d67ac76a8186672984bc49e
wuj <1450626722@qq.com>
2022-03-31 10:26:57 +0800

    ✨ feat(): 引入pinia

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,10 @@ import App from './App.vue';
 import './index.css';
 // 引入 vue-router
 import router from './router';
+// 引入 pinia
+import store from './store';
 
 const app = createApp(App);
 app.use(router);
+app.use(store);
 app.mount('#app');
